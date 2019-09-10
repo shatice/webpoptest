3a8c81427c4883fc2420595188fe6ad5d3bce66b
shatice <sarah.gungor@hetic.net>
2019-09-10 02:42:46 +0200

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: false,
+        loop: true,
+        autoplay: true,
+        margin: 50,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 2000,
+    });
+});
+
+$('.log-icon').click(function() {
+    $('.user-log').toggleClass('is-clicked'); 
+});
